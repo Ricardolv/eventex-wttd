20da3f440788498ac57d71e510540084adf46510
Ricardolv <richardluizv@gmail.com>
2016-01-10 15:49:20 -0200

    Incrições pagas

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -23,3 +23,8 @@ class SubscripitionModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Ricardo Luiz Viana', str(self.obj))
+
+    def test_paid_default_to_False(self):
+        """By default paid must be False."""
+        self.assertEqual(False, self.obj.paid)
+
